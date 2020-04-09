7f149fe2524eb93f3bc139339dbb6bcc8c7f310a
gromoval <gromov-rabota@yandex.ru>
2020-04-09 18:32:21 +0300

    [+] a lot of commits because of remote jenkins setting up

--- src/test/java/ru/lanit/atschool/steps/MainPageSteps.java
+++ src/test/java/ru/lanit/atschool/steps/MainPageSteps.java
@@ -102,6 +102,7 @@ public class MainPageSteps {
     public void логиныИПаролиПользователей(List<Map<String, String>> table) {
         for (int i=0; i<table.size(); i++) {
             new WebDriverWait(driver, 30).until(ExpectedConditions.elementToBeClickable(firstPage.btnSignIn));
+            new WebDriverWait(driver, 30).until(ExpectedConditions.visibilityOf(firstPage.btnSignIn));
             firstPage.btnSignIn.click();
             String username = table.get(i).get("login");
             String password = table.get(i).get("password");
